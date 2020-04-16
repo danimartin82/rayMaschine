617a9c1ad00492c11628859a3bc840e285c683d6
danimartin82 <elhombresinremedio@gmail.com>
2020-04-16 13:44:18 +0200

    Cleaning up the code

--- rayMaschine/rayMaschine.c
+++ rayMaschine/rayMaschine.c
@@ -54,9 +54,42 @@ const int windowHeight = 512;
 const int gameScreenWidth = 32;
 const int gameScreenHeight = 32;
 
+float frequencies[4][16] = 
+     {{   41.20,   43.65,   46.25,   49.00,   51.91,   55.00,   58.27,   61.74,   65.41,   69.30,   73.42,   77.78,   82.41,   87.31,   92.50,   98.00},
+      {  103.83,  110.00,  116.54,  123.47,  130.81,  138.59,  146.83,  155.56,  164.81,  174.61,  185.00,  196.00,  207.65,  220.00,  233.08,  246.94},
+      {  261.63,  277.18,  293.66,	311.13,	 329.63,  349.23,  369.99,	392.00,	 415.30,  440.00,  466.16,  493.88,	 523.25,  554.37,  587.33,  622.25},
+      {  659.25,  698.46,  739.99,  783.99,  830.61,  880.00,  932.33,  987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98}};
+    
+/*
+         E1,  F1, F#1,  G1, G#1,  A1, A#1,  B1,  C2, C#2,  D2, D#2,  E2,  F2, F#2,  G2
+        G#2,  A2, A#2,  B2,  C3, C#3,  D3, D#3,  E3,  F3, F#3,  G3, G#3,  A3, A#3,  B3
+         C4, C#4,  D4, D#4,  E4,  F4, F#4,  G4, G#4,  A4, A#4,  B4,  C5, C#5,  D5, D#5
+         E5,  F5, F#5,  G5, G#5,  A5, A#5,  B5,  C6, C#6,  D6, D#6,  E6,  F6, F#6,  G6
+
+*/
+
+int selector1 = 0;
+int selector2 = 0;
+#define zRows 4
+#define zCols 4
+#define numberZones  zRows* zCols
+int Yoffset = 4;
+int timers[numberZones] = { 0 };
+Color zColors[numberZones];
+Rectangle zones[numberZones];
+Color colorGUI = { 0, 153, 255, 255 };
+Color colorGUI2 = { 255, 0, 102, 255 };
+
+int samplerate = 22050;
+AudioStream stream;
 
 /*------------------------------------- Function headers -------------------------------*/
+void detect_user_interaction(void);
+void draw_user_interaction(void);
+void play_user_interaction(void);
+void draw_GUI(void);
 void generate_sin(short audio_samples[], int samples_length, int samplerate, float frequency);
+void generate_square(short audio_samples[], int samples_length, int samplerate, float frequency);
 void generate_hihat(short audio_samples[], int samples_length);
 Color highlightColor(Color input, int i);
 Color lowlightColor(Color input, int i);
@@ -79,91 +112,11 @@ int main(void)
     SetTextureFilter(target.texture, FILTER_POINT);  // Texture scale filter to use!
 
     // ------------ YOUR CODE here -------------
-    int samplerate = 22050;
     InitAudioDevice();
-    AudioStream stream = InitAudioStream(samplerate, 16, 1);
-    float frequencies[4][16] = {
-        {41.20, // E1
-        43.65, // F1
-        46.25, // F#1
-        49.00, // G1
-        51.91, // G#1 
-        55.00, // A1
-        58.27, // A#1
-        61.74, // B1
-        65.41, // C2
-        69.30, // C#2
-        73.42, // D2
-        77.78, // D#2
-        82.41, // E2
-        87.31, // F2
-        92.50, // F#2
-        98.00}, //	G2
-
-        {103.83, //	G#2
-        110.00, //	A2
-        116.54, //	A#2
-        123.47, //	B2
-        130.81, //	C3
-        138.59, //	C#3
-        146.83, //	D3
-        155.56, //	D#3
-        164.81, //	E3
-        174.61, //	F3
-        185.00, //	F#3
-        196.00, //	G3
-        207.65, //	G#3
-        220.00, //	A3
-        233.08, //	A#3
-        246.94 }, //B3
-
-        {261.63,//C4
-        277.18,	//C#4
-        293.66,	//D4
-        311.13,	//D#4
-        329.63,	//E4
-        349.23,	//F4
-        369.99,	//F#4
-        392.00,	//G4
-        415.30,	//G#4
-        440.00,	//A4
-        466.16,	//A#4
-        493.88,	//B4
-        523.25,	//C5
-        554.37,	//C#5
-        587.33,	//D5
-        622.25},//D#5
-
-        {659.25,//	E5
-        698.46,	//  F5
-        739.99,	//	F#5
-        783.99,	//	G5
-        830.61,	//	G#5 
-        880.00,	//	A5
-        932.33,	//	A#5
-        987.77,	//	B5
-        1046.50, //	C6
-        1108.73, //	C#6
-        1174.66, //	D6
-        1244.51, //	D#6
-        1318.51, //	E6
-        1396.91, //	F6
-        1479.98, //	F#6 
-        1567.98} //	G6
-
-    };
-
-    int selector1 = 0;
-    int selector2 = 0;
-    #define zRows 4
-    #define zCols 4
-    #define numberZones  zRows* zCols
-    int Yoffset = 4;
+    stream = InitAudioStream(samplerate, 16, 1);
+    
     int zWidth = gameScreenWidth / zCols;
-    int zHeight = (gameScreenHeight- Yoffset/ zRows) / zRows;
-    int timers[numberZones] = { 0 };
-    Color zColors[numberZones];
-    Rectangle zones[numberZones];
+    int zHeight = (gameScreenHeight - Yoffset / zRows) / zRows;
 
     int n = 0;
 
@@ -177,8 +130,7 @@ int main(void)
         }
     }
 
-    Color colorGUI = { 0, 153, 255, 255 };
-    Color colorGUI2 = { 255, 0, 102, 255 };
+
     // ------------ end of YOUR CODE ------------------
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -205,80 +157,12 @@ int main(void)
                 // ------------ YOUR CODE here -------------
                 //TraceLog(LOG_INFO, FormatText("%d,%d", x, y));
 
-                int x = (int)GetMouseX()/scale;
-                int y = (int)GetMouseY()/scale;
-                Color temp;
-             
-                // Detect if clicked zones
-                for (int i = 0; i < numberZones; i++)
-                {
-                    if ((x > zones[i].x) && (x < (zones[i].x + zones[i].width)) && (y > zones[i].y) && (y < (zones[i].y + zones[i].height)))
-                    {
-                        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-                        {
-                            timers[i] = 10;
-                        }
-                    }
-                }
-                // Detect if clicked Selector
-                if ((x >= 1) && (x <= 9) && (y >= 1) && (y <= 2) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-                {
-                    selector1++;
 
-                    if (selector1 == 4)
-                    {
-                        selector1 = 0;
-                    }
-                }
-                if ((x >= 12) && (x <= 19) && (y >= 1) && (y <= 2) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-                {
-                    selector2++;
+                detect_user_interaction();
+                draw_user_interaction();
+                play_user_interaction();
 
-                    if (selector2 == 3)
-                    {
-                        selector2 = 0;
-                    }
-                }
-
-
-                // Draw zones 
-                for (int i = 0; i < numberZones; i++)
-                {
-                    temp = zColors[i];
-
-                    if (timers[i] > 0)
-                    {
-                        temp = highlightColor(zColors[i], timers[i]);
-                        if (timers[i] == 10)
-                        {
-                            short audio_samples[MAX_SAMPLES] = { 0 };
-
-                             generate_sin(audio_samples, MAX_SAMPLES, samplerate, frequencies[selector1][i]);
-
-                            if (IsAudioStreamPlaying(stream))
-                            {
-                                StopAudioStream(stream);
-                            }
-                            UpdateAudioStream(stream, audio_samples, MAX_SAMPLES);
-                            PlayAudioStream(stream);
-                         }
-
-                        timers[i] -= 1;
-                    }
-   
-                    DrawRectangle(zones[i].x, zones[i].y, zones[i].width, zones[i].height, temp);
-       
-
-                }
-
-
-                DrawPixel(1, 1, colorGUI);
-                DrawLine(1, 2, 10, 2, colorGUI);
-                DrawLine(2 + 2 * selector1, 1, 4 + 2 * selector1, 1, colorGUI2);
-
-                DrawPixel(12, 1, colorGUI);
-                DrawLine(12, 2, 19, 2, colorGUI);
-                DrawLine(13 + 2 * selector2, 1, 15 + 2 * selector2, 1, colorGUI2);
+                draw_GUI();
 
      
                 // ------------ end of YOUR CODE ------------------
@@ -305,6 +189,139 @@ int main(void)
     CloseWindow();                  // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
     return 0;
+}
+
+
+
+
+/*--------------------------------------------------------------------------------------*/
+/*                                                                                      */
+/* Function: draw_GUI()                                                                 */
+/*                                                                                      */
+/*--------------------------------------------------------------------------------------*/
+void draw_GUI(void)
+{
+    DrawPixel(1, 1, colorGUI);
+    DrawLine(1, 2, 10, 2, colorGUI);
+    DrawLine(2 + 2 * selector1, 1, 4 + 2 * selector1, 1, colorGUI2);
+
+    DrawPixel(12, 1, colorGUI);
+    DrawLine(12, 2, 19, 2, colorGUI);
+    DrawLine(13 + 2 * selector2, 1, 15 + 2 * selector2, 1, colorGUI2);
+    return;
+}
+
+/*--------------------------------------------------------------------------------------*/
+/*                                                                                      */
+/* Function: detect_user_interction()                                                   */
+/*                                                                                      */
+/*--------------------------------------------------------------------------------------*/
+
+void detect_user_interaction(void)
+{
+
+    float scale = min((float)GetScreenWidth() / gameScreenWidth, (float)GetScreenHeight() / gameScreenHeight);
+    int x = (int)GetMouseX() / scale;
+    int y = (int)GetMouseY() / scale;
+
+    
+    // Detect if clicked zones
+    for (int i = 0; i < numberZones; i++)
+    {
+        if ((x > zones[i].x) && (x < (zones[i].x + zones[i].width)) && (y > zones[i].y) && (y < (zones[i].y + zones[i].height)))
+        {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                timers[i] = 10;
+            }
+        }
+    }
+    // Detect if clicked GUI
+    if ((x >= 1) && (x <= 9) && (y >= 1) && (y <= 2) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        selector1++;
+
+        if (selector1 == 4)
+        {
+            selector1 = 0;
+        }
+    }
+    if ((x >= 12) && (x <= 19) && (y >= 1) && (y <= 2) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        selector2++;
+
+        if (selector2 == 3)
+        {
+            selector2 = 0;
+        }
+    }
+    return;
+}
+
+/*--------------------------------------------------------------------------------------*/
+/*                                                                                      */
+/* Function: draw_user_interaction()                                                    */
+/*                                                                                      */
+/*--------------------------------------------------------------------------------------*/
+void draw_user_interaction(void)
+{
+    Color temp;
+    for (int i = 0; i < numberZones; i++)
+    {
+        temp = zColors[i];
+
+        if (timers[i] > 0)
+        {
+            temp = highlightColor(zColors[i], timers[i]);     
+        }
+
+        DrawRectangle(zones[i].x, zones[i].y, zones[i].width, zones[i].height, temp);
+
+
+    }
+    return;
+}
+
+/*--------------------------------------------------------------------------------------*/
+/*                                                                                      */
+/* Function: play_user_interaction()                                                    */
+/*                                                                                      */
+/*--------------------------------------------------------------------------------------*/
+void play_user_interaction(void)
+{
+
+    for (int i = 0; i < numberZones; i++)
+    {
+ 
+        if (timers[i] > 0)
+        {
+
+            if (timers[i] == 10)
+            {
+                short audio_samples[MAX_SAMPLES] = { 0 };
+
+                if (selector2 == 0)
+                {
+                    generate_sin(audio_samples, MAX_SAMPLES, samplerate, frequencies[selector1][i]);
+                }
+                else
+                {
+                    generate_square(audio_samples, MAX_SAMPLES, samplerate, frequencies[selector1][i]);
+                }
+                if (IsAudioStreamPlaying(stream))
+                {
+                    StopAudioStream(stream);
+                }
+                UpdateAudioStream(stream, audio_samples, MAX_SAMPLES);
+                PlayAudioStream(stream);
+            }
+
+            timers[i] -= 1;
+        }
+
+
+    }
+    return;
 }
 
 /*--------------------------------------------------------------------------------------*/
@@ -392,10 +409,32 @@ void generate_sin(short audio_samples[], int samples_length, int samplerate, flo
         int amplitude = 32000 - 8 * i;
 
         audio_samples[i] = (short)(sinf(((2 * PI * (float)i / waveLength))) * amplitude);
+      
     }
     return;
 }
-
+/*--------------------------------------------------------------------------------------*/
+/*                                                                                      */
+/* Function: generate_square()                                                          */
+/*                                                                                      */
+/*--------------------------------------------------------------------------------------*/
+void generate_square(short audio_samples[], int samples_length, int samplerate, float frequency)
+{
+    int waveLength = (int)(samplerate / frequency);
+    int number_waves = (int)(samples_length / waveLength);
+    for (int i = 0; i < waveLength * number_waves; i++)
+    {
+        int amplitude = 32000 - 8 * i;
+        int sign = 1;
+        if (i % waveLength > waveLength / 2)
+        {
+            sign = -1;
+        }
+        audio_samples[i] = amplitude * sign;
+      
+    }
+    return;
+}
 
 /*--------------------------------------------------------------------------------------*/
 /*                                                                                      */
